94d8b2f74ba286ebf2fac779dc62dd1a5d52bbfe
cclintris <181250083@smail.nju.edu.cn>
2022-08-29 21:39:43 -0400

    add: 0139. Word Break

--- 0139. Word Break/main.cpp
+++ 0139. Word Break/main.cpp
@@ -18,9 +18,53 @@ using namespace std;
 
 bool wordBreak(string s, vector<string>& wordDict);
 
+vector<int> nearestT(vector<bool>& dp, int end);
+
+void printVec(vector<bool>& vec);
+
 int main(int argc, char const* argv[]) {
+    string s = "applepenapple";
+    vector<string> wordDict{"apple", "pen"};
+    cout << wordBreak(s, wordDict) << endl;
     return 0;
 }
 
 bool wordBreak(string s, vector<string>& wordDict) {
+    int n = s.length();
+    vector<bool> dp(n, false);
+    for (int i = 0; i < n; i++) {
+        // cout << "i: " << i << endl;
+        // cout << s.substr(0, i + 1) << endl;
+        dp[i] = count(wordDict.begin(), wordDict.end(), s.substr(0, i + 1)) != 0 ? true : false;
+        // cout << "nearestT: " << nearestT(dp, i) << endl;
+        // cout << s.substr(nearestT(dp, i) + 1, i - nearestT(dp, i)) << endl;
+        vector<int> vec = nearestT(dp, i);
+        for (int j = 0; j < vec.size(); j++) {
+            int pos = vec[j];
+            dp[i] = dp[i] || count(wordDict.begin(), wordDict.end(), s.substr(pos + 1, i - pos)) != 0 ? true : false;
+        }
+        // printVec(dp);
+    }
+    // printVec(dp);
+    return dp[n - 1];
+}
+
+vector<int> nearestT(vector<bool>& dp, int end) {
+    vector<int> vec;
+    for (int i = end - 1; i >= 0; i--) {
+        if (dp[i]) vec.push_back(i);
+    }
+    return vec;
+}
+
+void printVec(vector<bool>& vec) {
+    int len = vec.size();
+    cout << "[";
+    for (int i = 0; i < len; i++) {
+        if (i == len - 1)
+            cout << vec[i];
+        else
+            cout << vec[i] << ',';
+    }
+    cout << "]" << endl;
 }
