98eb59abb8c3a5e4de98ed2609b38a984c155840
cclintris <181250083@smail.nju.edu.cn>
2022-09-02 14:19:32 -0400

    add: 0149. Max Points on a Line update

--- 0149. Max Points on a Line/main.cpp
+++ 0149. Max Points on a Line/main.cpp
@@ -30,20 +30,21 @@ int maxPoints(vector<vector<int>>& points) {
     int res = 0;
     for (int i = 0; i < points.size(); i++) {
         map<pair<int, int>, int> mp;
-        int duplicate = 1;
+        // int duplicate = 1;
         for (int j = i + 1; j < points.size(); j++) {
             int dx = points[i][0] - points[j][0];
             int dy = points[i][1] - points[j][1];
-            if (dx == 0 && dy == 0) {
-                duplicate++;
-                continue;
-            }
+            // if (dx == 0 && dy == 0) {
+            //     duplicate++;
+            //     continue;
+            // }
             int d = gcd(dx, dy);
             mp[{dx / d, dy / d}]++;
         }
-        res = max(res, duplicate);
+        // res = max(res, duplicate);
         for (map<pair<int, int>, int>::iterator it = mp.begin(); it != mp.end(); it++) {
-            res = max(res, it->second + duplicate);
+            res = max(res, it->second);
+            // res = max(res, it->second + duplicate);
         }
     }
     return res;
