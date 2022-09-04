f1845b085e5aaa630645c8314334a7fc428fb617
cclintris <181250083@smail.nju.edu.cn>
2022-09-04 18:29:09 -0400

    add: 0158. Read N Characters Given Read4 II - Call multiple times

---
+++ 0158. Read N Characters Given Read4 II - Call multiple times/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * I: file = "abc", queries = [1,2,1]
+ * O: [1,2,0]
+ * Explanation: The test case represents the following scenario:
+File file("abc");
+Solution sol;
+sol.read(buf, 1); // After calling your read method, buf should contain "a". We read a total of 1 character from the file, so return 1.
+sol.read(buf, 2); // Now buf should contain "bc". We read a total of 2 characters from the file, so return 2.
+sol.read(buf, 1); // We have reached the end of file, no more characters can be read. So return 0.
+Assume buf is allocated and guaranteed to have enough space for storing all characters from the file.
+ *
+ * I: file = "abc", queries = [4,1]
+ * O: [3,0]
+ * Explanation: The test case represents the following scenario:
+File file("abc");
+Solution sol;
+sol.read(buf, 4); // After calling your read method, buf should contain "abc". We read a total of 3 characters from the file, so return 3.
+sol.read(buf, 1); // We have reached the end of file, no more characters can be read. So return 0.
+ */
+
+int read4(char *buf);
+
+class Solution {
+   public:
+    int read(char *buf, int n) {
+        for (int i = 0; i < n; ++i) {
+            if (readPos == writePos) {
+                writePos = read4(buff);
+                readPos = 0;
+                if (writePos == 0) return i;
+            }
+            buf[i] = buff[readPos++];
+        }
+        return n;
+    }
+
+   private:
+    int readPos = 0, writePos = 0;
+    char buff[4];
+};
+int main(int argc, char const *argv[]) {
+    return 0;
+}
