3fcd64f2c971ab1c892ae0a22478ab6de4088daa
cclintris <181250083@smail.nju.edu.cn>
2022-10-20 22:04:02 -0400

    add: 0264. Ugly Number II

--- 0264. Ugly Number II/main.cpp
+++ 0264. Ugly Number II/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
 /**
@@ -15,8 +17,23 @@ using namespace std;
 int nthUglyNumber(int n);
 
 int main(int argc, char const *argv[]) {
+    int n = 10;
+    cout << nthUglyNumber(n) << endl;
     return 0;
 }
 
 int nthUglyNumber(int n) {
+    priority_queue<long long, vector<long long>, greater<long long>> minHeap;
+    minHeap.push(1);
+    for (int i = 1; i < n; i++) {
+        long long top = minHeap.top();
+        minHeap.pop();
+        while (minHeap.top() == top && !minHeap.empty()) {
+            minHeap.pop();
+        }
+        minHeap.push(top * 2);
+        minHeap.push(top * 3);
+        minHeap.push(top * 5);
+    }
+    return minHeap.top();
 }
